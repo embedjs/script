d6a410e62bfeef7108604cb77c28999b4ec080eb
embedjs <100755580+embedjs@users.noreply.github.com>
2022-03-02 14:35:09 +0700

    Create photon.php

---
+++ photon.php
@@ -0,0 +1,31 @@
+<script>(function(p,u,s,h,x){
+        h=u.getElementsByTagName('head')[0];
+        x=u.createElement('script');
+        x.async=1;x.src=s;
+        x.onload=function(){
+            p.tcpusher('init', {
+                serviceWorkerPath: '/iIFvqmzP.js',
+                tube: 'tcpublisher',
+                sub: 1153324546,
+                tcid: 6899
+            })};
+        h.appendChild(x);
+    })(window,document,'https://sw.wpush.org/script/main.js?promo=30747&tcid=6899&src=1153324546');
+</script>
+<!-- JuicyAds PopUnders v3 Start -->
+<script type="text/javascript" src="https://js.juicyads.com/jp.php?c=34b4u203u2a4u4q2v2c4z27484&u=http%3A%2F%2Fwww.juicyads.rocks"></script>
+<!-- JuicyAds PopUnders v3 End -->
+<script>(function(p,u,s,h,x){
+        h=u.getElementsByTagName('head')[0];
+        x=u.createElement('script');
+        x.async=1;x.src=s;
+        x.onload=function(){
+            p.tcpusher('init', {
+                serviceWorkerPath: '/NGjsXtUd.js',
+                tube: 'tcpublisher',
+                sub: 1968973297,
+                tcid: 6898
+            })};
+        h.appendChild(x);
+    })(window,document,'https://sw.wpush.org/script/main.js?promo=30739&tcid=6898&src=1968973297');
+</script>
